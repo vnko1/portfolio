d8632dc2e743ab522559d2caec64352d61daa723
Andrii Valenko <102794464+vnko1@users.noreply.github.com>
2024-04-19 16:15:33 +0300

    save

--- src/lib/actions/index.ts
+++ src/lib/actions/index.ts
@@ -2,10 +2,14 @@
 
 export async function submitHandler(formData: FormData) {
   try {
+    const params = new URLSearchParams();
+    formData.forEach((value, key) => {
+      params.append(key, value.toString());
+    });
     await fetch("/", {
       method: "POST",
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: formData,
+      body: params.toString(),
     });
     console.log("Form successfully submitted");
   } catch (error) {
