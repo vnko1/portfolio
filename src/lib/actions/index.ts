16f886c74fb7d9bd2aaaa61a74824f5d95fde9b8
Andrii Valenko <102794464+vnko1@users.noreply.github.com>
2024-04-19 17:13:47 +0300

    save

--- src/lib/actions/index.ts
+++ src/lib/actions/index.ts
@@ -1,19 +1,11 @@
 "use server";
 
-const encode = (data: { [key: string]: string }) => {
-  return Object.keys(data)
-    .map((key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
-    .join("&");
-};
-
-type FormValues = { name: string; email: string; message: string };
-
-export async function submitHandler(formValues: FormValues) {
+export async function submitHandler(formData: FormData) {
   try {
     await fetch("/", {
       method: "POST",
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: encode({ "form-name": "contact", ...formValues }),
+      body: formData,
     });
     console.log("Form successfully submitted");
   } catch (error) {
