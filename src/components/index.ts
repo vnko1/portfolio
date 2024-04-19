b14c02a0bf07c48f94391c5303032cc59a065e35
Andrii Valenko <102794464+vnko1@users.noreply.github.com>
2024-04-19 13:32:09 +0300

    add

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,3 +3,4 @@ export { default as Menu } from "./Menu/Menu";
 export { default as Social } from "./Social/Social";
 export { default as Footer } from "./Footer/Footer";
 export { default as ProjectTitle } from "./ProjectTitle/ProjectTitle";
+export { default as ProjectDescription } from "./ProjectDescription/ProjectDescription";
