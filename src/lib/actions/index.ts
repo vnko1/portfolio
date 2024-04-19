7057a7af8b8a6d7e7db20a23a0ddcc395326c579
Andrii Valenko <102794464+vnko1@users.noreply.github.com>
2024-04-19 17:18:25 +0300

    save

--- src/lib/actions/index.ts
+++ src/lib/actions/index.ts
@@ -2,11 +2,12 @@
 
 export async function submitHandler(formData: FormData) {
   try {
-    await fetch("/", {
-      method: "POST",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: formData,
-    });
+    console.log("🚀 ~ submitHandler ~ formData:", formData);
+    // await fetch("/", {
+    //   method: "POST",
+    //   headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    //   body: formData,
+    // });
     console.log("Form successfully submitted");
   } catch (error) {
     console.log("🚀 ~ submitHandler ~ error:", error);
