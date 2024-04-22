188a9176b39634bdf47bb53597bb9b00a7086ef4
Andrii Valenko <102794464+vnko1@users.noreply.github.com>
2024-04-22 12:08:07 +0300

    add metrics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Source_Code_Pro } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import "../styles/css/style.css";
 import "../styles/scss/index.scss";
 import { Footer, Header } from "@/components";
@@ -26,6 +28,8 @@ export default function RootLayout({
         <Header />
         {children}
         <Footer />
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
