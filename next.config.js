5aa33083bfc9a7420797663b0f201003e78aac1a
Andrii Valenko <102794464+vnko1@users.noreply.github.com>
2024-11-19 19:20:21 +0200

    added: env

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  env: { BASE_URL: process.env.BASE_URL, FORM_URL: process.env.FORM_URL },
+  env: {
+    BASE_URL: process.env.BASE_URL,
+    FORM_URL: process.env.FORM_URL,
+  },
 };
 
 module.exports = nextConfig;
