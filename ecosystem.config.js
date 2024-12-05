5f272dce4ba7eb3161bf044afa310db3b993b13a
Andrii Valenko <102794464+vnko1@users.noreply.github.com>
2024-12-05 17:12:39 +0200

    changed: ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -22,7 +22,7 @@ module.exports = {
       "post-deploy":
         "source ~/.nvm/nvm.sh && npm install && npm run build && pm2 reload ecosystem.config.js --env production",
       "pre-setup": "",
-      ssh_options: "ForwardAgent=yes",
+      // ssh_options: "ForwardAgent=yes",
     },
   },
 };
