7b6f102907e66bb5ae6cd65808ef61bf887122ee
Andrii Valenko <102794464+vnko1@users.noreply.github.com>
2024-12-05 12:20:42 +0200

    added: eco

---
+++ ecosystem.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  apps: [
+    {
+      script: "npm start",
+    },
+  ],
+
+  deploy: {
+    production: {
+      key: "hostinger_key.pem",
+      user: "root",
+      host: "62.72.20.238",
+      ref: "origin/main",
+      repo: "https://github.com/vnko1/portfolio.git",
+      path: "/home/root",
+      "pre-deploy-local": "",
+      "post-deploy":
+        "source ~/.nvm/nvm.sh && npm install && npm run build && pm2 reload ecosystem.config.js --env production",
+      "pre-setup": "",
+      ssh_options: "ForwardAgent=yes",
+    },
+  },
+};
