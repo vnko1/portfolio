dd4879c192106876a323a6fc104cb454ca9f857b
Andrii Valenko <andreyvalenko@gmail.com>
2025-02-19 07:04:58 +0200

    added: slider base markup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
-import { Home } from "@/components";
+import { Home, Slider } from "@/components";
 
 export default function HomePage() {
   return (
     <section id="section" className="section">
       <Home />
+      <Slider />
     </section>
   );
 }
